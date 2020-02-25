515727733fdb36cc57ee355d759d2c7a95d9d1a6
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-02-25 10:35:19 -0500

    Updates

---
+++ precache-manifest.d8f8f4abff0cae9d44c473e5cca79ede.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4915717ac274be874f0c9d1fec473489",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/index.html"
+  },
+  {
+    "revision": "b1ad2fc047ee3910cd69",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "511118e0faeab9756d98",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/css/main.9edffa66.chunk.css"
+  },
+  {
+    "revision": "b1ad2fc047ee3910cd69",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/js/2.55fdb914.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/js/2.55fdb914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511118e0faeab9756d98",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/js/main.67f0101a.chunk.js"
+  },
+  {
+    "revision": "9d9b8be25caaaa568d98",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/js/runtime-main.141fe0e2.js"
+  },
+  {
+    "revision": "1a632d2b39f3275752f2c4b3c73477a3",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/media/AngularLogo.1a632d2b.png"
+  },
+  {
+    "revision": "f38a1d6b713a32bf07bb384c59da3300",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/media/DiplomaNodeJS.f38a1d6b.PNG"
+  },
+  {
+    "revision": "e0abb9bc131e0f124a5cee65e545a05d",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/media/DiplomaPython.e0abb9bc.PNG"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/media/PythonLogo.b9bbb1d7.png"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/gventuraagramonte/portfolio-linkedin.git/static/media/ReactLogo.c3ba82a5.png"
+  }
+]);
